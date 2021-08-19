4a6990e875e5c45b362f3e3caa2d565f7ca1c10d
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-19 16:06:23 -0500

    Created Bills Table

--- database.sql
+++ database.sql
@@ -8,3 +8,23 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "bills" (
+    "id" SERIAL PRIMARY KEY,
+    "mortgage_rent" INT (100) DEFAULT 0,
+    "car_loan" INT (100) DEFAULT 0,
+    "furnace_loan" INT (100) DEFAULT 0,
+    "prime_loan" INT (100) DEFAULT 0,
+    "smaller_credit_card" INT (100) DEFAULT 0,
+    "bigger_credit_card" INT (100) DEFAULT 0,
+    "partner_credit_card" INT (100) DEFAULT 0,
+    "groceries" INT (100) DEFAULT 0,
+    "gas_car" INT (100) DEFAULT 0,
+    "gas_home" INT (100) DEFAULT 0,
+    "electric" INT (100) DEFAULT 0,
+    "auto_home_insurance" INT (100) DEFAULT 0,
+    "internet" INT (100) DEFAULT 0,
+    "trash" INT (100) DEFAULT 0,
+    "dog_food" INT (100) DEFAULT 0,
+    "gym" INT (100) DEFAULT 0,
+)
