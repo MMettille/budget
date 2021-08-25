880cd0daa607a37915b202ed965619273f5a7c1a
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-25 17:20:50 -0500

    Created Popovers for the inputs

--- src/components/W4/Tax.jsx
+++ src/components/W4/Tax.jsx
@@ -13,6 +13,9 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
 import FormLabel from '@material-ui/core/FormLabel';
 import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -33,7 +36,16 @@ function Tax() {
   const classes = useStyles();
   const [maritalStatus, setMaritalStatus] = useState("single");
   const [dependents, setDependents] = useState(1);
+  const [otherDependents, setOtherDependents] = useState(1);
   const [allowances, setAllowances] = useState(1);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <div className="container tax-box">
@@ -62,17 +74,76 @@ function Tax() {
           </RadioGroup>
         </FormControl>
         <h5>Claim Dependents</h5>
-        <p>If your total income will be $200,000 or less ($400,000 if married filing jointly):</p>
       <FormControl variant="outlined" className={classes.margin}>
-          <InputLabel htmlFor="new-amount">Number of dependents</InputLabel>
+          <InputLabel htmlFor="new-amount">Dependents</InputLabel>
           <OutlinedInput
             id="new-amount"
             value={dependents}
             onChange={(event) => setDependents(event.target.value)}
-            labelWidth={175}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            labelWidth={125}
             type="number"
+            onClick={(event) => setAnchorEl(event.currentTarget)}
           />
         </FormControl>
+       
+        <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>
+        Enter your annual estimated child tax credit.
+        This is typically $2,000 for each of your of children under age 17 that are your dependents.
+        The IRS instructions are to include this credit only if your income is $200,000 or less ($400,000 or less if married filing jointly).
+        If you have a spouse that works or have two jobs you should only claim these credits with one employer/paycheck.
+        </Typography>
+      </Popover>
+
+      <FormControl variant="outlined" className={classes.margin}>
+          <InputLabel htmlFor="new-amount">Other Dependents</InputLabel>
+          <OutlinedInput
+            id="new-amount"
+            value={otherDependents}
+            onChange={(event) => setOtherDependents(event.target.value)}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            labelWidth={125}
+            type="number"
+            onClick={(event) => setAnchorEl(event.currentTarget)}
+          />
+        </FormControl>
+       
+        <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>
+        Enter your annual estimated credit for other dependents.
+        This is typically $500 for each dependent that qualifies. Do not include children under age 17. 
+        The IRS instructions are to include this credit only if your income is $200,000 or less ($400,000 or less if married filing jointly). 
+        If you have a spouse that works or have two jobs you should only claim these credits with one employer/paycheck.
+        </Typography>
+      </Popover>
+
       <h3>State of Minnesota</h3>
       <FormControl variant="outlined" className={classes.margin}>
           <InputLabel htmlFor="new-amount">Allowances</InputLabel>
